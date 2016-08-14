c866e230b05cd776407f4ab1c5a31e32a1ea3748
nishant <nishant.singh87@gmail.com>
2016-08-14 20:39:42 +0530

    fix of cors from client.

--- src/server.rb
+++ src/server.rb
@@ -6,6 +6,10 @@ require_relative 'service'
 CONFIG_PATH = "config"
 service = RubyLab::Service.new CONFIG_PATH
 set :allow_origin, :any
+set :allow_methods,:any
+set :allow_credentials, true
+set :max_age, "1728000"
+set :expose_headers, ['Content-Type']
 
 put '/task/:task_id/evaluate' do
   solution_string = request.body.read;
