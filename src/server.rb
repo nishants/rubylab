bccca8c1b24a7493545852db43cbd75cbc37a5c8
nishant <nishant.singh87@gmail.com>
2016-08-14 20:53:47 +0530

    fix of cors from client.

--- src/server.rb
+++ src/server.rb
@@ -1,24 +1,17 @@
 require 'sinatra'
-require 'sinatra/cross_origin'
-
 require_relative 'service'
 
 CONFIG_PATH = "config"
 service = RubyLab::Service.new CONFIG_PATH
-set :allow_origin, :any
-set :allow_methods,:any
-set :allow_credentials, true
-set :max_age, "1728000"
-set :expose_headers, ['Content-Type']
 
-options "*" do
-  response.headers["Allow"] = "HEAD,GET,PUT,DELETE,OPTIONS"
-
-  # Needed for AngularJS
-  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-
-  halt HTTP_STATUS_OK
+before do
+  content_type :json
+  headers 'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+          'Access-Control-Allow-Headers' => 'Content-Type'
 end
+
+set :protection, false
 
 put '/task/:task_id/evaluate' do
   solution_string = request.body.read;
