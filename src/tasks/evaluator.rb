069e0218defcab24fb9c1307054d7e7a8b546921
nishant <nishant.singh87@gmail.com>
2016-08-16 00:19:52 +0530

    fixture for error message.

--- src/tasks/evaluator.rb
+++ src/tasks/evaluator.rb
@@ -18,7 +18,7 @@ module RubyLab
     end
 
     def error(exception)
-      @error = exception.message.incude?("for #") ? exception.message.split("for #")[1] : exception.message;
+      @error = exception.message.split("for #")[0];
       report
     end
 
