bc0567147268848ca79063e258c5ab5da02d069e
nishant <nishant.singh87@gmail.com>
2017-01-03 15:55:10 +0530

    tests files in test/data.

--- test/support/support.rb
+++ test/support/support.rb
@@ -4,27 +4,27 @@ module RubyLab
   class TestSupport
 
     def submission_for(task_id)
-      File.read("./config/tasks/#{task_id}/validation/submission.rb")
+      File.read("./test/data/submission.rb")
     end
 
     def failing_submission_for(task_id)
-      File.read("./config/tasks/#{task_id}/validation/failing_submission.rb")
+      File.read("./test/data/failing_submission.rb")
     end
 
     def expected_for(task_id)
-      JSON.parse(File.read("./config/tasks/#{task_id}/validation/expected.json"))
+      JSON.parse(File.read("./test/data/expected.json"))
     end
 
     def expected_failure_for(task_id)
-      JSON.parse(File.read("./config/tasks/#{task_id}/validation/expected_failure.json"))
+      JSON.parse(File.read("./test/data/expected_failure.json"))
     end
 
     def error_submission_for(task_id)
-      File.read("./config/tasks/#{task_id}/validation/error_submission.rb")
+      File.read("./test/data/error_submission.rb")
     end
 
     def expected_error_for(task_id)
-      JSON.parse(File.read("./config/tasks/#{task_id}/validation/expected_error.json"))
+      JSON.parse(File.read("./test/data/expected_error.json"))
     end
 
   end
